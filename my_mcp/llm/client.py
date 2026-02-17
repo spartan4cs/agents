93462873afb2aa7d08fb9553c8f7b15f743ff401
Akash Kumbhare <aakashkumbhare@gmail.com>
2026-02-17 22:33:40 +0530

    Update

--- my_mcp/llm/client.py
+++ my_mcp/llm/client.py
@@ -1,8 +1,17 @@
+import os
 from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()  # loads .env
 
 class LLMClient:
     def __init__(self):
-        self.client = Groq()
+        api_key = os.getenv("GROQ_API_KEY")
+
+        if not api_key:
+            raise ValueError("GROQ_API_KEY not found in environment")
+
+        self.client = Groq(api_key=api_key)
 
     def chat(self, messages, tools=None):
         return self.client.chat.completions.create(
